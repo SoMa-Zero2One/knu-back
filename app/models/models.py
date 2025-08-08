6396bac09bc544c7ef99b3df6d7244059ddf0e3e
Wibaek Park <devmuromi@gmail.com>
2025-08-08 16:52:39 +0900

    fix: sqlalchemy 서버 시작 시간으로 default 시간이 적용되는 오류 수정

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
-from datetime import UTC, datetime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
 from app.core.database import Base
 
@@ -13,9 +12,9 @@ class User(Base):
     grade = Column(Float, nullable=False)
     lang = Column(String(255), nullable=False)
     modify_count = Column(Integer, default=4, nullable=False)
-    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
     updated_at = Column(
-        DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC), nullable=False
+        DateTime, default=func.now(), onupdate=func.now(), nullable=False
     )
 
     applications = relationship("Application", back_populates="user")
@@ -28,8 +27,10 @@ class PartnerUniversity(Base):
     country = Column(String(255), nullable=False)
     slot = Column(Integer, nullable=False)
     duration = Column(String(255), nullable=False)  # "1개학기" or "2개학기"
-    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
-    updated_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(
+        DateTime, default=func.now(), onupdate=func.now(), nullable=False
+    )
 
     applications = relationship("Application", back_populates="university")
 
@@ -42,8 +43,10 @@ class Application(Base):
         Integer, ForeignKey("partner_university.id"), nullable=False
     )
     choice = Column(Integer, nullable=False)
-    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
-    updated_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(
+        DateTime, default=func.now(), onupdate=func.now(), nullable=False
+    )
 
     user = relationship("User", back_populates="applications")
     university = relationship("PartnerUniversity", back_populates="applications")
