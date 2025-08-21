5f735464fb73dd50522b077a4ac13c41476af4ce
Wibaek Park <devmuromi@gmail.com>
2025-08-21 15:30:16 +0900

    feat: CORS 변경

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 origins = [
     "https://soma-woad.vercel.app",
-    "https://gnu.gyohwan.com",
+    "https://knu.gyohwan.com",
     "http://localhost:3000",
     "http://localhost:8000",
 ]
